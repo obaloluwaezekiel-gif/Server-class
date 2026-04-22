2a3095cb30a397465e86bc21eae766b537402091
obaloluwaezekiel-gif <obaloluwaezekiel@gmail.com>
2026-04-22 04:23:47 +0100

    Server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+//app.use(express.json())
+
+const profile = [
+    {name: "Oba", age:5, indigine: "Osun"},
+    {name: "Pemi", age:5, indigene: "Osun"}
+]
+app.get('/', function(req, res){
+    res.send('Hello, World! Welcome to Express.js!')
+});
+
+app.get("/profilepage", function(request, response){
+    response.json(profile[0])
+})
+
+app.get("/about", (req, res)=>{
+    res.send("This is about page")
+})
+
+app.get('/contact', (request, response)=>{
+    response.send("This is contact page")
+})
+app.listen(3000, function(){
+    console.log('Server is up and running')
+});
